be4819930b1f1091d72eb651d3f732ee8bbb7b3a
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-06-18 23:13:55 +0200

    add missing useThemeOptions

---
+++ scripts/components/settings/use-theme-options.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from '@wordpress/element';
+import { toast } from 'sonner';
+import apiFetch from '@wordpress/api-fetch';
+
+export const useThemeOptions = (settingName = 'eightshift-theme-options') => {
+	const [settings, setSettings] = useState();
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		apiFetch({ path: '/wp/v2/settings' }).then((settings) => {
+			setSettings(settings?.[settingName]);
+		});
+	}, []);
+
+	const saveSettings = async () => {
+		setIsLoading(true);
+
+		try {
+			await apiFetch({
+				path: '/wp/v2/settings',
+				method: 'POST',
+				data: {
+					[settingName]: settings,
+				},
+			});
+
+			toast.success(__('Saved successfully', 'fe-libs-tailwind'));
+		} catch (error) {
+			toast.error(__('Something went wrong while saving', 'fe-libs-tailwind'), {
+				description: error.message,
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const updateSettings = (modified) => {
+		setSettings((prev) => ({
+			...prev,
+			...modified,
+		}));
+	};
+
+	return {
+		isLoading,
+		settings,
+		saveSettings,
+		setSettings,
+		updateSettings,
+	};
+};
