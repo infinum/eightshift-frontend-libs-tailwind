73aad913a7771ef9457c5930ca22af40619ef76c
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-06-26 09:09:48 +0200

    update exports

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,22 @@
 // Components.
-export { BlockInserter } from './components/block-inserter';
-export { ManageFileButton, FileSelector } from './components/file-picker';
-export { MediaPicker } from './components/media-picker';
-export { ServerSideRender } from './components/server-side-render';
-export {
-	ThemeOptionsPage,
-	EsThemeOptionsContext,
-} from './components/settings/settings';
+export { BlockInserter } from './components/block-inserter.js';
+export { ManageFileButton, FileSelector } from './components/file-picker.js';
+export { MediaPicker } from './components/media-picker.js';
+export { ServerSideRender } from './components/server-side-render.js';
+export { ThemeOptionsPage, EsThemeOptionsContext } from './components/settings/settings.js';
 
 // Editor.
-export * from './editor/attributes';
-export * from './editor/colors';
-export * from './editor/editor';
-export * from './editor/fetch';
-export * from './editor/options';
-export * from './editor/utility';
-export * from './editor/tailwindcss';
-export {
-	getAttributes,
-	getExample,
-	getFullBlockName,
-	getFullBlockNameVariation,
-	registerBlocks,
-	registerVariations,
-} from './editor/registration';
-export {
-	STORE_NAME,
-	BUILD_VERSION,
-	setStore,
-	setStoreGlobalWindow,
-	setConfigFlags,
-} from './editor/store';
+export * from './editor/attributes.js';
+export * from './editor/colors.js';
+export * from './editor/editor.js';
+export * from './editor/fetch.js';
+export * from './editor/options.js';
+export * from './editor/utility.js';
+export * from './editor/tailwindcss.js';
+export { getAttributes, getExample, getFullBlockName, getFullBlockNameVariation, registerBlocks, registerVariations } from './editor/registration.js';
+export { STORE_NAME, BUILD_VERSION, setStore, setStoreGlobalWindow, setConfigFlags } from './editor/store.js';
 
 // Helpers.
-export {
-	getBreakpointData,
-	getBreakpointNames,
-	getGlobalManifest,
-	getBreakpointUiData,
-	getResponsiveData,
-} from './helpers/breakpoints';
-export { cookies } from './helpers/cookies';
-export { dynamicImport } from './helpers/dynamic-import';
+export { getBreakpointData, getBreakpointNames, getGlobalManifest, getBreakpointUiData, getResponsiveData } from './helpers/breakpoints.js';
+export { cookies } from './helpers/cookies.js';
+export { dynamicImport } from './helpers/dynamic-import.js';
